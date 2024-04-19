bc3b4b993edfbb0b9dda32734e293d1f52a36652
PhucDT <dinhthaiphuc463@gmail.com>
2024-04-19 07:11:35 +0700

    create project java

---
+++ BTTuan1/src/main/java/tuan1/bttuan1/BTTuan1.java
@@ -0,0 +1,13 @@
+
+package tuan1.bttuan1;
+
+/**
+ *
+ * @author Admin
+ */
+public class BTTuan1 {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
